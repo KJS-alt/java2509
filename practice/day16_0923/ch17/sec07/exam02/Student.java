f47d817ec9639aa123bcebb3cd0f5cc64f621c57
KJS-alt <sh170kw@naver.com>
2025-09-23 23:39:54 +0900

    0923

---
+++ practice/day16_0923/ch17/sec07/exam02/Student.java
@@ -0,0 +1,13 @@
+package ch17.sec07.exam02;
+
+public class Student {
+    public String name;
+    public int score;
+
+    public Student(String name, int score) {
+        this.name = name;
+        this.score = score;
+    }
+    public String getName() {return name;}
+    public int getScore() {return score;}
+}
